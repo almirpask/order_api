5fb2fab99b2b7a079cfc5522f79729a69ce71ead
almirpask <almirpask@hotmail.com>
2024-01-30 19:46:19 -0300

    add class validations

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,9 +1,34 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsPositive,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
+
 export class CreateOrderDto {
+  @ArrayNotEmpty()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDtop)
   items: OrderItemDtop[];
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
   card_hash: string;
 }
 
 export class OrderItemDtop {
+  @IsPositive()
+  @IsInt()
+  @IsNotEmpty()
   quantity: number;
+
+  @MaxLength(36)
+  @IsString()
+  @IsNotEmpty()
   product_id: string;
 }
