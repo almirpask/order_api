c19b503ab646e5e740705f2ffd871f08c3a7943f
almirpask <almirpask@hotmail.com>
2024-01-30 18:35:23 -0300

    add products resource

---
+++ src/products/products.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+  ) {} // @InjectRepository(Products.Product<Product>)
+  create(createProductDto: CreateProductDto) {
+    const product = this.productRepo.create(createProductDto);
+    return this.productRepo.save(product);
+  }
+
+  findAll() {
+    return this.productRepo.find({});
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
