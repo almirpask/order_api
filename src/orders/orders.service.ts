e8e96e34e86de896405cd79e59ba561572bcc666
almirpask <almirpask@hotmail.com>
2024-01-30 21:52:49 -0300

    publish order messages in rabbitmq queue

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -4,12 +4,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
 import { In, Repository } from 'typeorm';
 import { Product } from 'src/products/entities/product.entity';
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order) private orderRepo: Repository<Order>,
     @InjectRepository(Product) private productRepo: Repository<Product>,
+    private amqpConnection: AmqpConnection,
   ) {}
   async create(createOrderDto: CreateOrderDto & { client_id: number }) {
     const productIds = createOrderDto.items.map((item) => item.product_id);
@@ -39,6 +41,13 @@ export class OrdersService {
     });
 
     await this.orderRepo.save(order);
+
+    await this.amqpConnection.publish('amq.direct', 'OrderCreated', {
+      order_id: order.id,
+      card_hash: createOrderDto.card_hash,
+      total: order.total,
+    });
+    console.log('order created');
     return order;
   }
 
